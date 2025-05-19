9435cb7102aef7a0a0a13a21669091b217d1eea0
Shalu Jha <183721575+Shalujha0907@users.noreply.github.com>
2025-05-19 10:26:16 +0530

    add dummy test

---
+++ src/Dummy.java
@@ -0,0 +1,10 @@
+public class Dummy {
+
+    public String printHello() {
+        return "Hello and welcome!";
+    }
+
+    public String printBye() {
+        return "Bye and goodbye!";
+    }
+}
---
+++ src/DummyTest.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DummyTest {
+    @Test
+    void pass() {
+        assert true ;
+    }
+
+    @Test
+    void printHello() {
+        Dummy dummy = new Dummy();
+        assertEquals("Hello and welcome!", dummy.printHello());
+    }
+
+    @Test
+    void printBye() {
+        Dummy dummy = new Dummy();
+        assertEquals("Bye and goodbye!", dummy.printBye());
+    }
+}
