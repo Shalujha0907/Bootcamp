96aa3590c786d448b3397bc7b96c0bd8b169ff9d
Shalu Jha <183721575+Shalujha0907@users.noreply.github.com>
2025-05-19 15:50:57 +0530

    add compliment method

---
+++ src/math/Probability.java
@@ -0,0 +1,40 @@
+package math;
+
+import java.util.Objects;
+
+public  class Probability {
+    private final double chance;
+
+    public Probability(double chance) {
+        this.chance = chance;
+    }
+
+    public static Probability initialise(double chance) throws Exception {
+        if (chance < 0 || chance > 1) {
+            throw new Exception("Invalid Chance");
+        }
+
+        return new Probability(chance);
+    }
+
+    public static Probability complement(double chance) throws Exception {
+    return  Probability.initialise(1 - chance);
+    }
+
+    public Probability and(Probability p2) {
+        return new Probability(chance * p2.chance);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (!(o instanceof Probability)) return false;
+        Probability that = (Probability) o;
+        return Double.compare(chance, that.chance) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(chance);
+    }
+
+}
---
+++ src/math/ProbabilityTest.java
@@ -0,0 +1,23 @@
+package math;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProbabilityTest {
+    @Test
+    void probability() throws Exception {
+      assertThrows(Exception.class, () -> Probability.initialise(-2));
+      assertEquals(Probability.initialise(0.75), Probability.complement(0.25));
+      assertEquals(Probability.initialise(0.5), Probability.complement(0.5));
+    }
+
+    @Test
+    void probability2() {
+        Probability p1 = new Probability(0.5);
+        Probability p2 = new Probability(0.5);
+
+        assertEquals(new Probability(0.25), p1.and(p2));
+    }
+
+}
